e96f133743ad938c228bdd4b1f70de05bdbbde7d
Na Hyejin <randall09@vers.kr>
2023-01-25 16:16:15 +0900

    setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { darkTheme } from './theme';
+
+const GlobalStyle = createGlobalStyle`
+body{
+  background-color: ${({ theme }) => theme.bgColor};
+}
+*{margin: 0; padding: 0; box-sizing:border-box; color:${({ theme }) =>
+  theme.textColor};
+font-family  :${({ theme }) => theme.fontFamilly} ;
+}
+li{list-style: none;}
+a{color:inherit; text-decoration: none;}
+
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/theme.tsx
@@ -0,0 +1,9 @@
+import { DefaultTheme } from 'styled-components';
+
+export const darkTheme: DefaultTheme = {
+  bgColor: '#000000',
+  accentColor: '#FB2576',
+  textColor: '#8fdfff',
+  tabColor: '#3F0071',
+  fontFamilly: "'Mukta', sans-serif;",
+};
